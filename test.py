072de29d393aff2b2a74ca86b87422f45d51ea0f
tandreeva <atm.nimeria@gmail.com>
2020-12-17 23:58:58 +0300

    test 2 done

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ def test():
 
     tree_conf = {
      "name":"1second",
-     "range":"1s",
+     "range":"5000s",
      "delta":"1s"
      }
     
@@ -15,91 +15,156 @@ def test():
                       "dst",
                       [ "src", "dst" ]
                     ],
-                    [ [ "src", [ "dst" ]],
-                      [ "dst", [ "src" ]]
+                    [ "src",
+                      "dst",
+                      [ "src", "dst" ],
+                      [ "src", "service" ],
+                      [ "dst", "service" ],
+                      [ "src", "dst", "service" ]
+                    ],
+                    [ "src",
+                      "dst",
+                      [ "src", "dst" ],
+                      [ "src", "proxy_src_ip" ],
+                      [ "src", "proxy_src_ip" ],
+                      [ "src", "service" ],
+                      [ "dst", "service" ],
+                      [ "dst", "proxy_src_ip" ],
+                      [ "service", "proxy_src_ip" ],
+                      [ "src", "dst", "service" ],
+                      [ "src", "dst", "proxy_src_ip" ],
+                      [ "src", "service", "proxy_src_ip" ],
+                      [ "dst", "service", "proxy_src_ip" ],
+                      [ "src", "dst", "service", "proxy_src_ip" ]
                     ]
+                   
                   ]
 
+    params_rel = [[['src', 'src & dst'],
+                   ['dst', 'src & dst']
+                  ],
+                  [['src', 'src & dst'],
+                   ['dst', 'src & dst'],
+                   ['src', 'src & service'],
+                   ['service', 'src & service'],
+                   ['dst', 'dst & service'],
+                   ['service', 'dst & service'],
+                   ['src', 'src & dst & service'],
+                   ['dst', 'src & dst & service'],
+                   ['service', 'src & dst & service'],
+                   ['src & dst', 'src & dst & service'],
+                   ['src & service', 'src & dst & service'],
+                   ['dst & service', 'src & dst & service'],
+                  ],
+                  [['src', 'src & dst'],
+                   ['src', 'src & service'],
+                   ['src', 'src & proxy_src_ip'],
+                   ['dst', 'src & dst'],
+                   ['dst', 'dst & service'],
+                   ['dst', 'dst & proxy_src_ip'],
+                   ['service', 'src & service'],
+                   ['service', 'dst & service'],
+                   ['service', 'service & proxy_src_ip'],
+                   ['proxy_src_ip', 'src & proxy_src_ip'],
+                   ['proxy_src_ip', 'dst & proxy_src_ip'],
+                   ['proxy_src_ip', 'service & proxy_src_ip'],
+                   ['src', 'src & dst & service'],
+                   ['src', 'src & dst & proxy_src_ip'],
+                   ['src', 'src & service & proxy_src_ip'],
+                   ['dst', 'src & dst & service'],
+                   ['dst', 'src & dst & proxy_src_ip'],
+                   ['dst', 'dst & service & proxy_src_ip'],
+                   ['service', 'src & dst & service'],
+                   ['service', 'src & service & proxy_src_ip'],
+                   ['service', 'dst & service & proxy_src_ip'],
+                   ['proxy_src_ip', 'src & dst & proxy_src_ip'],
+                   ['proxy_src_ip', 'src & service & proxy_src_ip'],
+                   ['proxy_src_ip', 'dst & service & proxy_src_ip'],
+                   ['src & dst', 'src & dst & service'],
+                   ['src & service', 'src & dst & service'],
+                   ['dst & service', 'src & dst & service'],
+                   ['src & dst', 'src & dst & proxy_src_ip'],
+                   ['src & proxy_src_ip', 'src & dst & proxy_src_ip'],
+                   ['dst & proxy_src_ip', 'src & dst & proxy_src_ip'],
+                   ['src & service', 'src & service & proxy_src_ip'],
+                   ['src & proxy_src_ip', 'src & service & proxy_src_ip'],
+                   ['service & proxy_src_ip', 'src & service & proxy_src_ip'],
+                   ['dst & service', 'dst & service & proxy_src_ip'],
+                   ['dst & proxy_src_ip', 'dst & service & proxy_src_ip'],
+                   ['service & proxy_src_ip', 'dst & service & proxy_src_ip'],
+                   ['src', 'src & dst & service & proxy_src_ip'],
+                   ['dst', 'src & dst & service & proxy_src_ip'],
+                   ['service', 'src & dst & service & proxy_src_ip'],
+                   ['proxy_src_ip', 'src & dst & service & proxy_src_ip'],
+                   ['src & dst', 'src & dst & service & proxy_src_ip'],
+                   ['src & service', 'src & dst & service & proxy_src_ip'],
+                   ['src & proxy_src_ip', 'src & dst & service & proxy_src_ip'],
+                   ['dst & service', 'src & dst & service & proxy_src_ip'],
+                   ['dst & proxy_src_ip', 'src & dst & service & proxy_src_ip'],
+                   ['service & proxy_src_ip', 'src & dst & service & proxy_src_ip'],
+                   ['src & dst & service', 'src & dst & service & proxy_src_ip'],
+                   ['src & dst & proxy_src_ip', 'src & dst & service & proxy_src_ip'],
+                   ['dst & service & proxy_src_ip', 'src & dst & service & proxy_src_ip'],
+                  ]
+                 ]
+
+                     
     data_path = 'data'
     
-    ranges = [ 10, 50, 100, 500, 1000, 5000, 10000, 15000, 20000, 25000, 30000 ]
-    n_files = [ 1,  1,   1,   1,    1,    4,     8,    11,    15,    19,    22 ]
-
+    n_files = 4
     cnt_tests = 3
 
-    result = dict.fromkeys(ranges,
-                           dict.fromkeys([agg.__name__ for agg in [agg0, agg1, agg2, agg3]],
-                                         dict.fromkeys(['agg_time', 'sum_size', 'avg_size', 'abs_time', 'rel_time'],
-                                                       None
-                                                      )
-                                        )
-                          )
     f = open('test_result.txt', 'w')
 
-    for time_range, n in zip(ranges, n_files):
-
-        tree_conf['range'] = f'{time_range}s'
-
-        for agg, i in zip([agg0, agg1, agg2, agg3], [ 0, 1, 1, 0 ]):
-
-            print(f'{datetime.datetime.now()} -> {agg.__name__} (time_range={time_range}, {n} files)')
-            result[time_range][agg.__name__]['agg_time'] = datetime.timedelta(0)
-
-            for cnt, (tree, td) in enumerate(agg.aggregate(tree_conf, params_conf[i], data_path)):
-
-                result[time_range][agg.__name__]['agg_time'] += td
-
-                if cnt + 1 == n:
+    for conf, relative in zip(params_conf, params_rel):
+        for agg in [agg0, agg3]:
+        
+            print(f'{datetime.datetime.now()} -> {agg.__name__} (conf)')
+            agg_time = datetime.timedelta(0)
+        
+            for cnt, (tree, td) in enumerate(agg.aggregate(tree_conf, conf, data_path)):
+        
+                agg_time += td
+        
+                if cnt + 1 == n_files:
                     
                     # time to aggregate 500000 elements
-                    result[time_range][agg.__name__]['agg_time'] /= n
-                    result[time_range][agg.__name__]['agg_time'] = int(result[time_range][agg.__name__]['agg_time'].total_seconds() * 1000)  # ms
-                    print(f'{time_range}, {agg.__name__}, agg_time, {result[time_range][agg.__name__]["agg_time"]}')
-                    f.write(f'{time_range}, {agg.__name__}, agg_time, {result[time_range][agg.__name__]["agg_time"]}\n')
+                    agg_time /= n_files
+                    agg_time = int(agg_time.total_seconds() * 1000)  # ms
+                    print(f'{agg.__name__}, agg_time, {agg_time}')
+                    f.write(f'{agg.__name__},agg_time,{agg_time}\n')
                     f.flush()
-
-                    # sum size of queues
-                    result[time_range][agg.__name__]['sum_size'] = asizeof.asizeof(tree.tree.queue) + (asizeof.asizeof(tree.tree.graph) if agg.__name__ == agg3.__name__ else 0)
-                    print(f'{time_range}, {agg.__name__}, sum_size, {result[time_range][agg.__name__]["sum_size"]}')
-                    f.write(f'{time_range}, {agg.__name__}, sum_size, {result[time_range][agg.__name__]["sum_size"]}\n')
-                    f.flush()
-                    
-                    # average size of queue
-                    result[time_range][agg.__name__]['avg_size'] = int(result[time_range][agg.__name__]['sum_size'] / len(tree.tree.queue))
-                    print(f'{time_range}, {agg.__name__}, avg_size, {result[time_range][agg.__name__]["avg_size"]}')
-                    f.write(f'{time_range}, {agg.__name__}, avg_size, {result[time_range][agg.__name__]["avg_size"]}\n')
-                    f.flush()
-
+        
                     # filter absolute
-                    result[time_range][agg.__name__]['abs_time'] = datetime.timedelta(0)
+                    abs_time = datetime.timedelta(0)
                     for _ in range(cnt_tests):
                         tm = datetime.datetime.now()
                         tree.filter(['1second'],
-                                    absolute=[['src', ''], ['dst', '']])
-                        result[time_range][agg.__name__]['abs_time'] += (datetime.datetime.now() - tm)
-                    result[time_range][agg.__name__]['abs_time'] /= cnt_tests
-                    result[time_range][agg.__name__]['abs_time'] = result[time_range][agg.__name__]['abs_time'].total_seconds() * 1000
-                    print(f'{time_range}, {agg.__name__}, abs_time, {result[time_range][agg.__name__]["abs_time"]}')
-                    f.write(f'{time_range}, {agg.__name__}, abs_time, {result[time_range][agg.__name__]["abs_time"]}\n')
+                                    absolute=[['', '']])
+                        abs_time += (datetime.datetime.now() - tm)
+                    abs_time /= cnt_tests
+                    abs_time = abs_time.total_seconds() * 1000
+                    print(f'{agg.__name__}, abs_time, {abs_time}')
+                    f.write(f'{agg.__name__},abs_time,{abs_time}\n')
                     f.flush()
-
+        
                     # filter relative
-                    result[time_range][agg.__name__]['rel_time'] = datetime.timedelta(0)
+                    rel_time = datetime.timedelta(0)
                     for _ in range(cnt_tests):
                         tm = datetime.datetime.now()
                         tree.filter(['1second'],
                                     relative=[['src', 'src & dst'],
                                               ['dst', 'src & dst']
                                              ])
-                        result[time_range][agg.__name__]['rel_time'] += (datetime.datetime.now() - tm)
-                    result[time_range][agg.__name__]['rel_time'] /= cnt_tests
-                    result[time_range][agg.__name__]['rel_time'] = result[time_range][agg.__name__]['rel_time'].total_seconds() * 1000
-                    print(f'{time_range}, {agg.__name__}, rel_time, {result[time_range][agg.__name__]["rel_time"]}')
-                    f.write(f'{time_range}, {agg.__name__}, rel_time, {result[time_range][agg.__name__]["rel_time"]}\n')
+                        rel_time += (datetime.datetime.now() - tm)
+                    rel_time /= cnt_tests
+                    rel_time = rel_time.total_seconds() * 1000
+                    print(f'{agg.__name__}, rel_time, {rel_time}')
+                    f.write(f'{agg.__name__},rel_time,{rel_time}\n')
                     f.flush()
-
+        
                     print('')
-
+        
                     break
 
     f.close()
