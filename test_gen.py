5f065328b2d4b4ae6fe12a0123d8cea93cd64e13
tandreeva <atm.nimeria@gmail.com>
2020-12-21 22:00:33 +0300

    fix test with get

--- test_gen.py
+++ test_gen.py
@@ -1,87 +1,95 @@
 import datetime
 from random import randint
+import pandas as pd
 
 import agg0, agg3
 from pympler import asizeof
 
-def generate(params_cnt: int, values_cnt: int = 100):
+def generate(params_cnt: int = 10, values_cnt: int = 50):
     result = dict()
-    for p in range(params_cnt):
-        p_rand = randint(1, params_cnt)
+    for p in range(1, params_cnt + 1):
         v_rand = randint(1, values_cnt)
-        result[f'p{p_rand}'] = v_rand
+        result[f'p{p}'] = v_rand
     return result
 
 def test():
 
+    time_range = 500
+
     tree_conf = {
      "name":"1second",
-     "range":"10000s",
+     "range":f"{time_range}s",
      "delta":"1s"
      }
     
     cnt_tests = 5
-    cnt_params = 10
+    cnt_params = 12
 
     params_conf = []
     params_rel = []
 
-    for n in range(2, cnt_params + 1):
+    for n in range(2, cnt_params):
         conf = [f'p{i}' for i in range(1, n + 1)]
         params_conf.append(conf + [conf])
         confs = ' & '.join(conf)
-        for i in range(1, n + 1):
-            rel = [[f'p{i}', confs]]
-            params_rel.append(rel)
+        rel = [[f'p{i}', confs] for i in range(1, n + 1)]
+        params_rel.append(rel)
 
-    for n in range(2, cnt_params + 1):
-        print(params_conf[n])
+    tree0 = [agg0.AggTree(tree_conf, params) for params in params_conf]
+    tree3 = [agg3.AggTree(tree_conf, params) for params in params_conf]
 
-    for n in range(2, cnt_params + 1):
-        print(params_rel[n])
+    dt = pd.to_datetime('19Dec2020 19:00:00', errors='coerce', format='%d%b%Y %H:%M:%S')
+    packet = dict()
 
-    return
+    for i in range(time_range + 1):
 
-    f = open('res4.csv', 'w')
+        print(f'{datetime.datetime.now()} -> {i}')
 
-    for agg in [agg0, agg3]:
-    
-        tree = AggTree(tree_conf, params_conf)
+        packets_per_second = randint(200, 300)
 
-        print(f'{datetime.datetime.now()} -> {agg.__name__} (conf)')
-        agg_time = datetime.timedelta(0)
-    
-        for cnt, (tree, td) in enumerate(agg.aggregate(tree_conf, conf, data_path)):
-    
-            agg_time += td
-    
-            if cnt + 1 == n_files:
-                
-                # time to aggregate 100000 elements
-                agg_time = int(agg_time.total_seconds() * 1000)  # ms
-                print(f'{agg.__name__}, agg_time, {agg_time}')
-                f.write(f'{agg.__name__},agg_time,{agg_time}\n')
-                f.flush()
-    
-                # filter relative
-                rel_time = datetime.timedelta(0)
-                for _ in range(cnt_tests):
-                    tm = datetime.datetime.now()
-                    tree.filter(['1second'],
-                                relative=[['src', 'src & dst'],
-                                          ['dst', 'src & dst']
-                                         ])
-                    rel_time += (datetime.datetime.now() - tm)
-                rel_time /= cnt_tests
-                rel_time = rel_time.total_seconds() * 1000
-                print(f'{agg.__name__}, rel_time, {rel_time}')
-                f.write(f'{agg.__name__},rel_time,{rel_time}\n')
-                f.flush()
-    
-                print('')
-    
-                break
+        for j in range(packets_per_second):
 
+            packet = generate(cnt_params)
+            packet['datetime'] = dt
+
+            #for tree in tree0:
+            #    tree.aggregate(packet)
+
+            for tree in tree3:
+                tree.aggregate(packet)
+
+        dt += datetime.timedelta(seconds=1)
+
+    # filter relative
+    f = open('test_result.txt', 'w')
+
+    #for i, (tree, rel) in enumerate(zip(tree0, params_rel)):
+    #    rel_time = datetime.timedelta(0)
+    #    for _ in range(cnt_tests):
+    #        tm = datetime.datetime.now()
+    #        tree.filter(['1second'],
+    #                    relative=rel)
+    #        rel_time += (datetime.datetime.now() - tm)
+    #    rel_time /= cnt_tests
+    #    rel_time = rel_time.total_seconds() * 1000
+    #    print(f'    {i+2}: {len(tree.tree.queue)}')
+    #    print(f'agg0, {i+2}, rel_time, {rel_time}')
+    #    f.write(f'agg0,{i+2},rel_time,{rel_time}\n')
+    #    f.flush()
+
+    for i, (tree, rel) in enumerate(zip(tree3, params_rel)):
+        rel_time = datetime.timedelta(0)
+        for _ in range(cnt_tests):
+            tm = datetime.datetime.now()
+            tree.filter(['1second'],
+                        relative=rel)
+            rel_time += (datetime.datetime.now() - tm)
+        rel_time /= cnt_tests
+        rel_time = rel_time.total_seconds() * 1000
+        print(f'agg3, {i+2}, rel_time, {rel_time}')
+        f.write(f'agg3,{i+2},rel_time,{rel_time}\n')
+        f.flush()
+    
     f.close()
 
 if __name__ == '__main__':
