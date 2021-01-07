1140300388fca96ddb3cb03d8d02091286d0434f
tandreeva <atm.nimeria@gmail.com>
2021-01-08 00:07:21 +0300

    fix test 3, add results for 20, 30, 40, 50 uniq

--- tests/test3_gen.py
+++ tests/test3_gen.py
@@ -5,14 +5,14 @@ import pandas as pd
 import agg, agg0, agg3
 from pympler import asizeof
 
-def generate(params_cnt, values_cnt: int = 20):
+def generate(params_cnt, values_cnt):
     result = dict()
     for p in range(1, params_cnt + 1):
         v_rand = randint(1, values_cnt)
         result[f'p{p}'] = v_rand
     return result
 
-def test():
+def test(uniq_params):
 
     time_range = 100
 
@@ -28,7 +28,7 @@ def test():
     params_conf = []
     params_rel = []
 
-    for n in range(2, cnt_params):
+    for n in range(2, cnt_params + 1):
         conf = [[f'p{i}'] for i in range(1, n + 1)]
         params_conf.append(conf + [[c[0] for c in conf]])
         confs = ' & '.join([c[0] for c in conf])
@@ -49,7 +49,7 @@ def test():
 
         for j in range(packets_per_second):
 
-            packet = generate(cnt_params)
+            packet = generate(cnt_params, uniq_params)
             packet['datetime'] = dt
 
             for tree in tree0:
@@ -60,7 +60,7 @@ def test():
 
         dt += timedelta(seconds=1)
 
-    f = open('results/test3.csv', 'w')
+    f = open(f'results/test3-{uniq_params}.csv', 'w')
 
     # filter relative
 
@@ -72,10 +72,17 @@ def test():
             rel_time += (datetime.now() - tm)
         rel_time /= cnt_tests
         rel_time = rel_time.total_seconds() * 1000
+        
         print(f'agg3,{i+2},cnt_queu,{len(tree.tree.queue)}')
         f.write(f'agg3,{i+2},cnt_queu,{len(tree.tree.queue)}\n')
+        
+        sum_size = asizeof.asizeof(tree.tree.queue) + asizeof.asizeof(tree.tree.graph)
+        print(f'agg3,{i+2},sum_size,{sum_size}')
+        f.write(f'agg3,{i+2},sum_size,{sum_size}\n')
+        
         print(f'agg3,{i+2},rel_time,{rel_time}')
         f.write(f'agg3,{i+2},rel_time,{rel_time}\n')
+        
         f.flush()
 
     for i, (tree, rel) in enumerate(zip(tree0, params_rel)):
@@ -86,14 +93,22 @@ def test():
             rel_time += (datetime.now() - tm)
         rel_time /= cnt_tests
         rel_time = rel_time.total_seconds() * 1000
+
         print(f'agg0,{i+2},cnt_queu,{len(tree.tree.queue)}')
         f.write(f'agg0,{i+2},cnt_queu,{len(tree.tree.queue)}\n')
+        
+        sum_size = asizeof.asizeof(tree.tree.queue)
+        print(f'agg0,{i+2},sum_size,{sum_size}')
+        f.write(f'agg0,{i+2},sum_size,{sum_size}\n')
+
         print(f'agg0,{i+2},rel_time,{rel_time}')
         f.write(f'agg0,{i+2},rel_time,{rel_time}\n')
+        
         f.flush()
     
     f.close()
 
 if __name__ == '__main__':
 
-    test()
+    for i in [20, 30, 40, 50]:
+        test(i)
